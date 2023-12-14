e0f7b946132efb4e523465e60105d343cd9ce7db
Kedar <kedar.don30@gmail.com>
2023-12-14 11:24:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/flashlightapp/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.flashlightapp;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleFlashLightOnOff;
+    private CameraManager cameraManager;
+    private String getCameraID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toggleFlashLightOnOff = findViewById(R.id.toggle_flashlight);
+
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+
+        try {
+
+            getCameraID = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void toggleFlashLight(View view) {
+        if (toggleFlashLightOnOff.isChecked()) {
+
+            try {
+                // true sets the torch in ON mode
+                cameraManager.setTorchMode(getCameraID, true);
+
+
+                Toast.makeText(MainActivity.this, "Flashlight is turned ON", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+
+                e.printStackTrace();
+            }
+        } else {
+
+            try {
+
+                cameraManager.setTorchMode(getCameraID, false);
+
+                Toast.makeText(MainActivity.this, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void finish() {
+        super.finish();
+        try {
+
+            cameraManager.setTorchMode(getCameraID, false);
+
+
+            Toast.makeText(this, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+        } catch (CameraAccessException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
